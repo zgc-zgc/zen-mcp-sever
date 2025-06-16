8c3efd56767cee7a89c835187abddd3a0a14a960
Fahad <fahad@2doapp.com>
2025-06-16 05:52:17 +0400

    Bump

--- config.py
+++ config.py
@@ -14,9 +14,9 @@ import os
 # These values are used in server responses and for tracking releases
 # IMPORTANT: This is the single source of truth for version and author info
 # Semantic versioning: MAJOR.MINOR.PATCH
-__version__ = "4.7.2"
+__version__ = "4.7.3"
 # Last update date in ISO format
-__updated__ = "2025-06-15"
+__updated__ = "2025-06-16"
 # Primary maintainer
 __author__ = "Fahad Gilani"
 
