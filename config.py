132c6ca0257355bfeee3fb5be50f20064325c62b
Fahad <fahad@2doapp.com>
2025-06-22 15:29:05 +0400

    secaudit - new tool for security audits

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ import os
 # These values are used in server responses and for tracking releases
 # IMPORTANT: This is the single source of truth for version and author info
 # Semantic versioning: MAJOR.MINOR.PATCH
-__version__ = "5.5.7"
+__version__ = "5.6.0"
 # Last update date in ISO format
 __updated__ = "2025-06-22"
 # Primary maintainer
